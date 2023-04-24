39524a859aa04bdd6253c615a8df0a9b633e4b69
Shikatastrophe <shikatastrophe@gmail.com>
2023-04-23 21:34:01 -0600

    Colliders q no funcionan
    Agregué colliders a las notas, pero  la neta no jalan y no entiendo pq

---
+++ Assets/NoteScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteScript : MonoBehaviour
+{
+
+    public GameObject NotaA;
+    public GameObject NotaB;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == NotaA)
+        {
+            Success();
+        }
+        else if (collision.gameObject == NotaB)
+        {
+            Fail();
+        }
+    }
+
+    private void Success()
+    {
+        Debug.Log("Gaming");
+    }
+
+    private void Fail()
+    {
+        Debug.Log("Fail");
+    }
+}
